c9668d46955a85d3e76b744180361da92888ba2c
JOSnow985 <jaden.olvera@students.snow.edu>
2026-02-18 02:46:11 -0700

    Implemented RN overrides and matching Unit Test

--- Lab05.Tests/UnitTest1.cs
+++ Lab05.Tests/UnitTest1.cs
@@ -7,7 +7,7 @@ public class Tests
     // Assert.Pass();
 
     [Test]  // Sets an attribute that our test is called when we run dotnet test
-    public void TwoPositiveTest()     // Tests that 12 and 6 produce the expected behavior
+    public void RNTwoPositiveTest()     // Tests that 12 and 6 produce the expected behavior
     {
         var test = new RationalNumber(12, 6);
         
@@ -15,6 +15,18 @@ public class Tests
         {
             Assert.That(test.Numerator, Is.EqualTo(2));
             Assert.That(test.Denominator, Is.EqualTo(1));
+        });
+    }
+    [Test]
+    public void RNOverrideTest()     // Tests that our overriden methods are working properly
+    {
+        var test = new RationalNumber(12, 6);
+        var testcompare = new RationalNumber(24, 12);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Equals(testcompare), Is.True);
+            Assert.That(test.GetHashCode().Equals(testcompare.GetHashCode()), Is.True);
         });
     }
 }
--- Lab05/Class1.cs
+++ Lab05/Class1.cs
@@ -21,6 +21,21 @@ public class RationalNumber
     public int Denominator => _denominator;
 
     // Methods
+    // Overriding Equals(object? obj) 
+    public override bool Equals(object? obj)
+    {   
+        RationalNumber? rn = obj as RationalNumber; // rn will be null if obj isn't a RationalNumber, null or not
+        // Check if rn is null first, then if the properties match. 
+        if (rn == null) 
+            return false;
+        else if (rn.Numerator != Numerator || rn.Denominator != Denominator) 
+            return false;
+        else
+            return true;
+    }
+    // Overriding GetHashCode(), uses both Numerator and Denominator
+    public override int GetHashCode() => HashCode.Combine(Numerator, Denominator);
+
     // Recursive Method to eventually return the greatest common denominator
     private static int GreatestCommonDenominator(int a, int b) => b == 0 ? Math.Abs(a) : GreatestCommonDenominator(b, a % b);
 }
