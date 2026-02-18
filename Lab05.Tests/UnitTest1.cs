49f6f76aaaec82edaa85caed3ac93bb225502c13
JOSnow985 <jaden.olvera@students.snow.edu>
2026-02-18 01:36:58 -0700

    Reworked test to be relevant for implementing RationalNumber

--- Lab05.Tests/UnitTest1.cs
+++ Lab05.Tests/UnitTest1.cs
@@ -6,15 +6,15 @@ public class Tests
     // public void Setup(){}
     // Assert.Pass();
 
-    [Test]
-    public void ClassTest()     // Tests our class has the properties and types
+    [Test]  // Sets an attribute that our test is called when we run dotnet test
+    public void TwoPositiveTest()     // Tests that 12 and 6 produce the expected behavior
     {
         var test = new RationalNumber(12, 6);
         
         Assert.Multiple(() =>
         {
-            Assert.That(test, Has.Property("Numerator").With.TypeOf<int>());
-            Assert.That(test, Has.Property("Denominator").With.TypeOf<int>());
+            Assert.That(test.Numerator, Is.EqualTo(2));
+            Assert.That(test.Denominator, Is.EqualTo(1));
         });
-    } 
+    }
 }
