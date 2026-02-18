19676398cd9f09d628fd24fa1fd04f4ebee632fd
JOSnow985 <jaden.olvera@students.snow.edu>
2026-02-18 00:37:51 -0700

    Unit Test for RationalNumber class

--- Lab05.Tests/UnitTest1.cs
+++ Lab05.Tests/UnitTest1.cs
@@ -8,8 +8,14 @@ public class Tests
     }
 
     [Test]
-    public void Test1()
+    public void ClassTest()     // Tests our class has the properties and types
     {
-        Assert.Pass();
-    }
+        var test = new RationalNumber(12, 6);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test, Has.Property("Numerator").With.TypeOf<int>());
+            Assert.That(test, Has.Property("Denominator").With.TypeOf<int>());
+        });
+    } 
 }
