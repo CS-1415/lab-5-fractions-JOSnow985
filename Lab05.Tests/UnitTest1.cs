270545a836ad8465344430590c2f92825dbf9e63
JOSnow985 <jaden.olvera@students.snow.edu>
2026-02-18 15:36:05 -0700

    Implemented all tests from assignment

--- Lab05.Tests/UnitTest1.cs
+++ Lab05.Tests/UnitTest1.cs
@@ -6,8 +6,8 @@ public class Tests
     // public void Setup(){}
     // Assert.Pass();
 
-    [Test]  // Sets an attribute that our test is called when we run dotnet test
-    public void RNTwoPositiveTest()     // Tests that 12 and 6 produce the expected behavior
+    [Test]  // Sets an attribute that our test can be run by dotnet test
+    public void RNTwoPositiveTest()     // Tests that (12/6) will return (2/1), Unit Test #1
     {
         var test = new RationalNumber(12, 6);
         
@@ -18,10 +18,101 @@ public class Tests
         });
     }
     [Test]
-    public void RNOverrideTest()     // Tests that our overriden methods are working properly
+    public void RNPositiveNegativeTest()     // Tests that (12/-6) will return (2/-1), Unit Test #2
     {
-        var test = new RationalNumber(12, 6);
-        var testcompare = new RationalNumber(24, 12);
+        var test = new RationalNumber(-12, 6);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Numerator, Is.EqualTo(2));
+            Assert.That(test.Denominator, Is.EqualTo(-1));
+        });
+    }
+    [Test]
+    public void RNNegativePositiveTest()     // Tests that (-12/6) will return (-2/1), Unit Test #3
+    {
+        var test = new RationalNumber(-12, 6);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Numerator, Is.EqualTo(-2));
+            Assert.That(test.Denominator, Is.EqualTo(1));
+        });
+    }
+    [Test]
+    public void RNTwoNegativeTest()     // Tests that (-12/-6) will return (2/1), Unit Test #4
+    {
+        var test = new RationalNumber(-12, -6);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Numerator, Is.EqualTo(2));
+            Assert.That(test.Denominator, Is.EqualTo(1));
+        });
+    }
+    [Test]
+    public void RNSimplifyTest()     // Tests that (60/3600) can be simplified to (1/60), Unit Test #5
+    {
+        var test = new RationalNumber(60, 3600);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Numerator, Is.EqualTo(1));
+            Assert.That(test.Denominator, Is.EqualTo(60));
+        });
+    }
+    [Test]
+    public void RNNoSimplifyTest()     // Tests that (6/25) won't be simplified, Unit Test #6
+    {
+        var test = new RationalNumber(6, 25);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Numerator, Is.EqualTo(6));
+            Assert.That(test.Denominator, Is.EqualTo(25));
+        });
+    }
+    [Test]
+    public void RNEqualTest()     // Tests RN Equals and GHC overrides, Unit Test #7
+    {
+        var test = new RationalNumber(20, 10);
+        var testcompare = new RationalNumber(4, 2);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.Equals(testcompare), Is.True);
+            Assert.That(test.GetHashCode().Equals(testcompare.GetHashCode()), Is.True);
+        });
+    }
+    [Test]
+    public void MNSimplifyTest()     // Tests that (25/6) is simplified to (4 1/6), Unit Test #8
+    {
+        var test = new MixedNumber(25, 6);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.WholeUnits, Is.EqualTo(4));
+            Assert.That(test.PartialUnits.Numerator, Is.EqualTo(1));
+            Assert.That(test.PartialUnits.Denominator, Is.EqualTo(6));
+        });
+    }
+    [Test]
+    public void MNNoWholeTest()     // Tests that (9/30) is simplified to (0 3/10), Unit Test #9
+    {
+        var test = new MixedNumber(9, 30);
+        
+        Assert.Multiple(() =>
+        {
+            Assert.That(test.WholeUnits, Is.EqualTo(0));
+            Assert.That(test.PartialUnits.Numerator, Is.EqualTo(3));
+            Assert.That(test.PartialUnits.Denominator, Is.EqualTo(10));
+        });
+    }
+    [Test]
+    public void MNEqualTest()     // Tests MN Equals and GHC overrides, Unit Test #10
+    {
+        var test = new MixedNumber(125, 50);
+        var testcompare = new MixedNumber(5, 2);
         
         Assert.Multiple(() =>
         {
