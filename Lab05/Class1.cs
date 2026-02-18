fb0f6a83db67ee356aae9b8148bf5b02e23a6134
JOSnow985 <jaden.olvera@students.snow.edu>
2026-02-18 01:37:46 -0700

    Implemented RationalNumber class, TwoPositiveTest passes

--- Lab05/Class1.cs
+++ Lab05/Class1.cs
@@ -1,6 +1,26 @@
 ﻿namespace Lab05;
 
-public class Class1
+public class RationalNumber
 {
+    // Fields
+    private readonly int _numerator;
+    private readonly int _denominator;
 
+    // Constructors
+    public RationalNumber(int numerator, int denominator)
+    {
+        // Find GCD
+        int gcd = GreatestCommonDenominator(numerator, denominator);
+        // Divide both numerator and denominator by gcd and set our fields
+        _numerator = numerator / gcd;
+        _denominator = denominator / gcd;
+    }
+
+    // Properties
+    public int Numerator => _numerator;
+    public int Denominator => _denominator;
+
+    // Methods
+    // Recursive Method to eventually return the greatest common denominator
+    private static int GreatestCommonDenominator(int a, int b) => b == 0 ? Math.Abs(a) : GreatestCommonDenominator(b, a % b);
 }
